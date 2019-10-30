0a7fbeea9faba18b4ee42d375478ea814bcd7411
Milica Stepanović <stepa.milica@gmail.com>
2019-10-30 14:50:53 +0100

    Ne znam da li je greška do mog koda, noda ili nečeg trećeg, ali ne vidim promene na sajtu. Plus ispravljen Šangelin iconic sugar daddy speech

--- Nedelja2/zadatak2/zadatak2.js
+++ Nedelja2/zadatak2/zadatak2.js
@@ -1,17 +1,9 @@
-var title = 'Welcome to DOM';
-document.getElementById('title').innerHTML = title;
+var title = "Welcome to DOM";
+document.getElementById("title").innerHTML = title;
 console.lotg(title);
 
-
-var text = `<p>Let's get it together before you wanna read. 
-I don't have a sugar daddy,
-I've never had a sugar daddy, everything I got I worked myself 
-and I need you to know that 100%. I don't have a suggar daddy, 
-I've never had a sugar daddy, if I wanted a sugar daddy - yes, 
-I could probably go and get one. Because I'm what? SICKENING! 
-YOU could never have a sugar dady, because 
-you-are-not-that-kinda-girl! I've never had a sugar daddy, 
-I've built myself from the ground up you 
-fUCKING <b>BITCH</b>!! </>p`;
 document.getElementById("pasus").innerHTML =
-text;
+`<p>Let's get it together before you wanna read. <br>I don't have a sugar daddy sweetheart, everything that I've had - I worked for and I worked for to get and I 
+built myself so I need you to know that 100%. <br> I don't have a suggar daddy, I've never had a sugar daddy, if I wanted a sugar daddy - yes, I could probably go and get one. 
+Because I'm what? <b>SICKENING</b>! <br> YOU could never have a sugar dady, because you-are-not-that-kinda-girl! Baby, everything I have I worked for and I 
+got mysel... baby, I've built myself from the ground up you fUCKING <b>BITCH</b>!! </>p`;
