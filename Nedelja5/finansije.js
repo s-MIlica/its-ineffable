91aa746c7b5e54ead263ab1b1f54f2236e2823bb
Milica Stepanović <stepa.milica@gmail.com>
2019-11-21 03:36:55 +0100

    urađen html, skoro završen css, i započet js

---
+++ Nedelja5/finansije.js
@@ -0,0 +1,4 @@
+const select = document.querySelector('select');
+const number = document.querySelector('number-input');
+const text = documentd.querySelector('text-input');
+const button = document.querySelector('butn');
