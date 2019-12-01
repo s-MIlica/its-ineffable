948e80df82a78c20067e5fc78466cba620791a90
Milica Stepanović <stepa.milica@gmail.com>
2019-12-01 16:17:08 +0100

    urađen i poslednji zadatak

--- Nedelja6/index.js
+++ Nedelja6/index.js
@@ -1,15 +1,5 @@
-// 1. Направити форму у HTML која се садржи од:
-//     * Username (text)
-//     * Password (password)
-//     * Confirm (Button)
-// 2. Направити JSON фајл који садржи информације о корисницима (низ објеката са Username и Password, креирајте макар 3 објекта)
-// 3. Направити следеће провере:
-//     * Username и password морају имати најмање 3 карактера и највише 15
-//     * Username не сме почети бројем
-// 4. Проверити да ли се унесени подаци налазе у датом JSON-у (иначе се то ради у Backend-у)
-// 5. Како бисмо ово урадили са Backend-ом који треба да прими информације из форме, и на основу тога нам враћа true/false? * само описати процес
 
-const username = document.querySelector("#username");
+const username = document.querySelector('#username');
 const password = document.querySelector("#password");
 const button = document.querySelector("#button");
 
@@ -17,20 +7,41 @@ button.addEventListener('click', (e) => {
     console.log(e.target);
     checkUsername()
     checkPassword()
+    checkJSON()
 });
 
 function checkUsername() {
-    if (username.length < 3 || username.length > 15) {
+    console.log(username.value);
+    if (username.value.length < 3 || username.value.length > 15) {
         return alert('Username must be between 3 and 15 characters long!');
     }
-    let first = username.charAt(0);
+    let first = username.value.charAt(0);
     if (isNaN(first) == false) {
         return alert("Username can't start with a number!");
     }
 };
 
 function checkPassword() {
-    if (password.length < 3 || password.length > 15) {
+    console.log(password.value);
+    if (password.value.length < 3 || password.value.length > 15) {
         return alert('Password must be between 3 and 15 characters long!');
     }
 };
+
+var getJSON = JSON.parse('[ {"Username": "Pera","Password": "pera123"},{"Username": "Mika","Password": "mika123"},{"Username": "Milojka","Password": "milojka123"}, {"Username": "Živka","Password": "Živka123"}]')
+console.log(getJSON);
+
+
+function checkJSON () {
+        getJSON.forEach (element => {
+            if (element.Username == username.value && element.Password == password.value) { 
+                return console.log(true)
+            } else {
+                    return console.log(false) 
+                    }
+        });   
+}
+
+// 5. Како бисмо ово урадили са Backend-ом који треба да прими информације из форме, и на основу тога нам враћа true/false? * само описати процес
+    /* Napravili bismo funkciju koja uzima podatke sa backend-a. Zatim bismo napravili novu funkciju koja prolazi kroz sve elemente niza i upoređeuje ih sa unetim username-om 
+    i password-om. Ako bi pronašap poklapanje vratio bi true, a ako ne vratio bi false */
